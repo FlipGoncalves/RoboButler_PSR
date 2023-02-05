9db9da27cc8f4ca489fc94860587b1cdb117e965
Rafael <rafaelfoliveira@ua.pt>
2023-02-05 17:58:42 +0000

    3rd task in menu working

--- psr_apartment_description/src/task_visualizer.py
+++ psr_apartment_description/src/task_visualizer.py
@@ -24,8 +24,6 @@ from actionlib_msgs.msg import GoalStatus
 pub = rospy.Publisher('visualization_marker', Marker, queue_size=10)
 
 
-#lista = []
-
 def parse_classes_file(path):
     classes = []
     with open(path, "r") as f:
@@ -57,7 +55,6 @@ def pose_callback(pose_msg, actual_pose):
     actual_pose[0] = pose_msg.pose.pose
     
 
-
 def image_callback(data, rospack, menu_msg, photo_taken):
     bridge = CvBridge()
     
@@ -81,22 +78,16 @@ def image_callback(data, rospack, menu_msg, photo_taken):
        
         cv2.imwrite(photo_path, image)
 
-def send_goal_thread(client, goal):
-    client.send_goal(goal)
-    client.wait_for_result()
-
 
 
 def callback_yolo(data, classes, id_list, places, menu_msg, goal_reached, moving_to_goal, look_for_obj, actual_pose, rot_over, obj_found):
-    client_2 = actionlib.SimpleActionClient("move_base", MoveBaseAction)
-    client_2.wait_for_server()
     msg = menu_msg[0]
     id_list =[]
     for detection in data.detections:
         # Get the id of each object
         object = detection.results[0].id
         id_list.append(object)
-    print(id_list)  
+
     # Task 1 - Move somewhere
     if msg in places.keys() and not goal_reached[0] and not moving_to_goal[0]:
         moving_to_goal[0] = True
@@ -107,54 +98,20 @@ def callback_yolo(data, classes, id_list, places, menu_msg, goal_reached, moving
         pub.publish(text_show)
     
     # Task 2 - Look for an object
-    if msg in classes: 
-
-        # Define the goal
-        for i in range(8):
-            goal = Pose()
-            goal.position = actual_pose[0].position
-            goal.orientation = Quaternion(*tf.transformations.quaternion_from_euler(0, 0, i * 45))
-       
-            rot_goal = MoveBaseGoal()
-            rot_goal.target_pose.header.frame_id = "map"
-            rot_goal.target_pose.pose = goal
-                
-            # Send the goal
-            thread = threading.Thread(target=send_goal_thread, args=(client_2, rot_goal))
+    if msg in classes:
+        if classes.index(msg) in id_list:
+            obj_found[0]=True
+            text_show = marker('Found it!')
+            pub.publish(text_show)
+        if not look_for_obj[0] and not obj_found[0]:
+            look_for_obj[0] = True
+            thread = threading.Thread(target=rot_goal, args=(msg, look_for_obj, actual_pose, obj_found, rot_over))
             thread.start()
-            # text = 'Looking for ' + str(msg)
-            # text_show = marker(text)
-            # pub.publish(text_show)
-            #print(client_2.get_state())
-            # Check for new object detections
-            rate = rospy.Rate(10.0000)
+        if rot_over[0]:
+            text_show = marker('Did not find any ' + msg)
+            pub.publish(text_show)
 
-           
-      
-        rospy.on_shutdown(client_2.cancel_all_goals)
-   
        
-       
-       
-       
-        # event = threading.Event()
-        # thread = threading.Thread(target=rot_goal, args=(msg, rot_over, look_for_obj, actual_pose, classes))
-        # thread.start()
-        # rot_goal(msg, rot_over, look_for_obj, actual_pose, classes)
-        # if classes.index(msg) in id_list:
-        # # look_for_obj[0] = False
-        #     print("found it!")
-        #     # look_for_obj[0] = False 
-        #     event.set()
-
-        # text_show = marker('Found it!')
-        # pub.publish(text_show)
-         
-    # elif rot_over[0]:
-    #     text_show = marker('There is no ' + msg)
-    #     pub.publish(text_show)
-
-      
     
     
 def callback_menu(data, menu_msg, photo_taken, goal_reached, moving_to_goal, look_for_obj, rot_over, obj_found):
@@ -203,13 +160,13 @@ def send_goal(places, target_name, goal_reached, moving_to_goal):
     rospy.on_shutdown(client.cancel_all_goals)
    
 
-def rot_goal(target_name, rot_over, look_for_obj, actual_pose, classes):
+def rot_goal(target_name, look_for_obj, actual_pose, obj_found, rot_over):
     client = actionlib.SimpleActionClient("move_base", MoveBaseAction)
     client.wait_for_server()
-       
-    
-   
+
+    rospy.Rate(5)   
     # Define the goal
+    n=0
     for i in range(8):
 
         goal = Pose()
@@ -219,31 +176,38 @@ def rot_goal(target_name, rot_over, look_for_obj, actual_pose, classes):
         rot_goal = MoveBaseGoal()
         rot_goal.target_pose.header.frame_id = "map"
         rot_goal.target_pose.pose = goal
-         
-        if classes.index(target_name) in id_list:
-            client.cancel_all_goals()    
-            break
-            
+
         # Send the goal
         if look_for_obj[0] == True:
+
             client.send_goal(rot_goal)
             text = 'Looking for ' + str(target_name)
             
-            text_show = marker(text)
-            pub.publish(text_show)
-            
+            while True:
+                text_show = marker(text)
+                pub.publish(text_show)
+                if obj_found[0] == True:
+                    client.cancel_all_goals() 
+                    break
 
-            client.wait_for_result()
-
+                if client.get_state() == GoalStatus.SUCCEEDED:
+                    break
+ 
+                if look_for_obj[0] == False:
+                    client.cancel_all_goals() 
+                    break  
         else:
             client.cancel_all_goals()    
             break
-  
+        n +=1
+        # print(n)
+        if n ==8 and not obj_found[0]:
+            rot_over[0]=True 
+    
+    rospy.sleep(1)     
     # Wait for the result
     client.wait_for_result()
     rospy.on_shutdown(client.cancel_all_goals)
-
-        
 
 
 def stop_publishing(): 
