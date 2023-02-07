8dd11a6f2dc7b735e47cf40fd9fe85566032c8d8
FlipGoncalves <filipeg@ua.pt>
2023-02-07 09:40:25 +0000

    changed move claw python file

--- psr_apartment_description/src/move_claw.py
+++ psr_apartment_description/src/move_claw.py
@@ -5,19 +5,22 @@ from std_msgs.msg import Float64
 
 def move_claw():
     
-    # between 1 and 0 -» 1 open / 0 closed
-    right = 1
-    # between -1 and 0 -» -1 open / 0 closed
-    left = -1
+    # right -> between 1 and 0 -» 1 open / 0 closed
+    # left -> between -1 and 0 -» -1 open / 0 closed
+    open = False
+    if open:
+        measure = 1
+    else:
+        measure = 0
     
     pub_right = rospy.Publisher('/robutler/pole_to_claw_right_second_joint_position_controller/command', Float64, queue_size=10)
     pub_left = rospy.Publisher('/robutler/pole_to_claw_left_second_joint_position_controller/command', Float64, queue_size=10)
     rospy.init_node('move_claw')
     rate = rospy.Rate(10) # 10hz
     count = 0
-    while count != 3:
-        pub_right.publish(float(right))
-        pub_left.publish(float(left))
+    while count != 5:
+        pub_right.publish(float(measure))
+        pub_left.publish(float(-measure))
         rate.sleep()
         count += 1
   
